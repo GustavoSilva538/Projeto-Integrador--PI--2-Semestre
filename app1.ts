bbfe30c80b9f63927872378dc11497243d5f68fa
Gustavo Silva <gustavosilva76887@gmail.com>
2026-08-11 11:17:41 -0300

    Add files via upload

--- app1.ts
+++ app1.ts
@@ -1,26 +1,77 @@
 import prompt from "prompt-sync";
 const sc = prompt();
 
-console.log("==================");
-console.log("====== Média =====");
+let loop : number = 0;
 
-console.log("Digite o primeiro Número:");
-let num1: number = Number(sc(""));
+console.log("====================");
+console.log("====== Dados ======");
+console.log("Digite seu Nome: ");
+let nome : String = sc();
 
-console.log("Digite o segundo Número:");
-let num2: number = Number(sc(""));
+console.log("Digite seu Sobrenome: ");
+let sobrenome : String = sc();
 
-console.log("Digite o terceiro Número:");
-let num3: number = Number(sc(""));
+console.log("Digite sua idade: ");
+let idade : number = Number(sc());
 
-let media: number = (num1 + num2 + num3) / 3;
 
-let situacao: string = "";
-if (media >= 6) {
-  situacao = "Aprovado";
-} else {
-  situacao = "Reprovado";
+while(loop == 0){
+
+for(let i : number = 0; i < 5; i++){
+            console.log("");
+        }
+
+console.log("====== MENU =====");
+console.log("1- Calcular Média");
+console.log("2- Dados Pessoais");
+console.log("3- Sair");
+console.log("====================");
+console.log("Oque vc deseja fazer: ");
+let escolha : number = Number(sc());
+
+
+switch(escolha){
+    case 1:
+        console.log("Digite a nota da primeira prova: ");
+        let n1 : number = Number(sc());
+
+        console.log("Digite a nota da segunda prova: ");
+        let n2 : number = Number(sc());
+
+        console.log("Digite a nota da terceira prova: ");
+        let n3 : number = Number(sc());
+
+        let media : number = (n1 + n2 + n3) /3 ;
+
+        
+        console.log("===================");
+
+        if (media >=6){
+            console.log("Aprovado");
+        }else{
+            console.log("Reprovado");
+        }
+        console.log("===================");
+
+
+    
+    case 2: 
+        
+        
+        console.log("===================");
+        console.log("Nome: " + nome + " " +sobrenome);
+        console.log("Idade: " + idade);
+        console.log("===================");
+        for(let i : number = 0; i < 5; i++){
+            console.log("");
+        }
+        break;
+
+    case 3:
+        loop = 1;
+        break;
 }
 
-console.log("Média: " + media);
-console.log("Situação: " + situacao);
+
+
+}
